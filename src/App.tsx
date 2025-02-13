0bd4e9ebd94c63d4ef71bf07cf50851855e52055
SajjadBayat <sajjad.bayat32@gmail.com>
2025-02-14 01:47:07 +0330

    refactor: make pages to load lazy and add fallback on top of Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, HotelPage } from "./pages";
 import AppLayout from "./layout/app-layout/AppLayout";
+import { lazy, Suspense } from "react";
+import { PageLoading } from "./components";
+
+const HomePage = lazy(() => import("./pages/home-page/HomePage"));
+const HotelPage = lazy(() => import("./pages/hotel-page/HotelPage"));
 
 function App() {
 	return (
 		<AppLayout>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/hotels/:id" element={<HotelPage />} />
-			</Routes>
+			<Suspense fallback={<PageLoading />}>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/hotels/:id" element={<HotelPage />} />
+				</Routes>
+			</Suspense>
 		</AppLayout>
 	);
 }
