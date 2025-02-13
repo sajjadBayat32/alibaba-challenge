cb31b828c103a08bd78adec169391cc972fc8676
SajjadBayat <sajjad.bayat32@gmail.com>
2025-02-14 01:00:13 +0330

    feat: add PageLoading component

---
+++ src/components/PageLoading/PageLoading.tsx
@@ -0,0 +1,11 @@
+function PageLoading() {
+	return (
+		<div className="w-70 h-40 rounded-lg bg-white">
+			<div className="flex items-center w-full  h-full justify-center">
+				<div className="font-bold">در حال بارگذاری...</div>
+			</div>
+		</div>
+	);
+}
+
+export default PageLoading;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { HotelCardItem } from "./HotelCardItem/HotelCardItem";
 export { default as HotelCardList } from "./HotelCardList/HotelCardList";
 export { default as HotelSearchBox } from "./HotelSearchBox/HotelSearchBox";
 export { default as MapViewer } from "./MapViewer/MapViewer";
+export { default as PageLoading } from "./PageLoading/PageLoading";
