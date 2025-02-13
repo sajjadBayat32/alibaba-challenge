bdafa6688edd0b891c4719e1fb5e8e54c0558409
SajjadBayat <sajjad.bayat32@gmail.com>
2025-02-14 01:00:33 +0330

    feat: add UserCommentBox component

---
+++ src/components/UserCommentBox/UserCommentBox.tsx
@@ -0,0 +1,17 @@
+import { UserComment } from "../../models";
+
+function UserCommentBox({ comment }: { comment: UserComment }) {
+	return (
+		<li
+			key={comment.id}
+			className="border border-gray-200 py-2 px-4 rounded-lg mb-2"
+		>
+			<div className="flex flex-col gap-2">
+				<strong>{comment.user}</strong>
+				<p>{comment.text}</p>
+			</div>
+		</li>
+	);
+}
+
+export default UserCommentBox;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as HotelCardList } from "./HotelCardList/HotelCardList";
 export { default as HotelSearchBox } from "./HotelSearchBox/HotelSearchBox";
 export { default as MapViewer } from "./MapViewer/MapViewer";
 export { default as PageLoading } from "./PageLoading/PageLoading";
+export { default as UserCommentBox } from "./UserCommentBox/UserCommentBox";
