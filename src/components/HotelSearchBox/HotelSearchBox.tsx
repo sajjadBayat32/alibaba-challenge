b6ef7bf7576a4bcd8c90f2097b4b9bbc12439fb0
sajjadBayat32 <sajjad.bayat32@gmail.com>
2025-02-14 23:03:30 +0330

    refactor: improve code readability by creating ComponentProps interfaces

--- src/components/HotelSearchBox/HotelSearchBox.tsx
+++ src/components/HotelSearchBox/HotelSearchBox.tsx
@@ -1,21 +1,20 @@
 import { ChangeEventHandler } from "react";
 
-function HotelSearchBox({
-	searchTerm,
-	callBack,
-}: {
-	searchTerm: string;
-	callBack: ChangeEventHandler<HTMLInputElement>;
-}) {
-	return (
-		<input
-			type="text"
-			placeholder="بین نام هتل ها و توضیحاتشون جستجو کن ..."
-			className="border p-2 rounded-lg w-full border-neutral-200 bg-white focus:border-neutral-500 focus-visible:outline-none"
-			value={searchTerm}
-			onChange={callBack}
-		/>
-	);
+function HotelSearchBox({ searchTerm, callBack }: ComponentProps) {
+  return (
+    <input
+      type="text"
+      placeholder="بین نام هتل ها و توضیحاتشون جستجو کن ..."
+      className="border p-2 rounded-lg w-full border-neutral-200 bg-white focus:border-neutral-500 focus-visible:outline-none"
+      value={searchTerm}
+      onChange={callBack}
+    />
+  );
+}
+
+interface ComponentProps {
+  searchTerm: string;
+  callBack: ChangeEventHandler<HTMLInputElement>;
 }
 
 export default HotelSearchBox;
--- src/components/MapViewer/MapViewer.tsx
+++ src/components/MapViewer/MapViewer.tsx
@@ -6,68 +6,66 @@ import { Hotel } from "../../models";
 import { memo, useEffect } from "react";
 
 const hotelIcon = icon({
-	iconUrl: "/icons/hotel-marker.svg",
-	iconSize: [30, 30],
-	className: "rounded",
+  iconUrl: "/icons/hotel-marker.svg",
+  iconSize: [30, 30],
+  className: "rounded",
 });
 
 const highlightedIcon = icon({
-	iconUrl: "/icons/selected-hotel-marker.svg",
-	iconSize: [30, 30],
+  iconUrl: "/icons/selected-hotel-marker.svg",
+  iconSize: [30, 30],
 });
 
 const MapViewer = memo(
-	({
-		hotels,
-		selectedHotelId,
-		onMarkerClick,
-	}: {
-		hotels: Hotel[];
-		selectedHotelId: number | null;
-		onMarkerClick: (id: number) => void;
-	}) => {
-		const latLngList: LatLngExpression[] = hotels.map((hotel) => [
-			hotel.location.lat,
-			hotel.location.long,
-		]);
-		return (
-			<MapContainer className="h-full w-full rounded-lg">
-				<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-				<MapController latLngList={latLngList} />
-				{hotels.map((hotel) => (
-					<Marker
-						key={hotel.id}
-						alt={hotel.name}
-						position={[hotel.location.lat, hotel.location.long]}
-						icon={selectedHotelId === hotel.id ? highlightedIcon : hotelIcon}
-						eventHandlers={{ click: () => onMarkerClick(hotel.id) }}
-					>
-						<Popup>
-							<div className="flex gap-2 items-center justify-between base-font">
-								<h2 className="font-bold">{hotel.name}</h2>
-								<div className="bg-yellow-500 px-2 h-5 flex items-center rounded-full">
-									<span className="top-0.5 relative">ستاره {hotel.stars}</span>
-								</div>
-							</div>
-						</Popup>
-					</Marker>
-				))}
-			</MapContainer>
-		);
-	}
+  ({ hotels, selectedHotelId, onMarkerClick }: ComponentProps) => {
+    const latLngList: LatLngExpression[] = hotels.map((hotel) => [
+      hotel.location.lat,
+      hotel.location.long,
+    ]);
+    return (
+      <MapContainer className="h-full w-full rounded-lg">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MapController latLngList={latLngList} />
+        {hotels.map((hotel) => (
+          <Marker
+            key={hotel.id}
+            alt={hotel.name}
+            position={[hotel.location.lat, hotel.location.long]}
+            icon={selectedHotelId === hotel.id ? highlightedIcon : hotelIcon}
+            eventHandlers={{ click: () => onMarkerClick(hotel.id) }}
+          >
+            <Popup>
+              <div className="flex gap-2 items-center justify-between base-font">
+                <h2 className="font-bold">{hotel.name}</h2>
+                <div className="bg-yellow-500 px-2 h-5 flex items-center rounded-full">
+                  <span className="top-0.5 relative">ستاره {hotel.stars}</span>
+                </div>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
+  }
 );
 
 function MapController({ latLngList }: { latLngList: LatLngExpression[] }) {
-	const map = useMap();
-	useEffect(() => {
-		if (latLngList.length === 1) {
-			map.setView(latLngList[0], 15);
-		} else if (latLngList.length > 1) {
-			const maxBound = new LatLngBounds(latLngList);
-			map.fitBounds(maxBound);
-		}
-	}, [latLngList, map]);
-	return null;
+  const map = useMap();
+  useEffect(() => {
+    if (latLngList.length === 1) {
+      map.setView(latLngList[0], 15);
+    } else if (latLngList.length > 1) {
+      const maxBound = new LatLngBounds(latLngList);
+      map.fitBounds(maxBound);
+    }
+  }, [latLngList, map]);
+  return null;
+}
+
+interface ComponentProps {
+  hotels: Hotel[];
+  selectedHotelId: number | null;
+  onMarkerClick: (id: number) => void;
 }
 
 export default MapViewer;
