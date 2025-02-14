3ab8ed1bcdafc182a68a6f5a0350a7e054a3601f
sajjadBayat32 <sajjad.bayat32@gmail.com>
2025-02-14 23:01:49 +0330

    feat: add not-found page for dead routes and null hotel

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import { Route, Routes } from "react-router-dom";
 import AppLayout from "./layout/app-layout/AppLayout";
 import { lazy, Suspense } from "react";
 import { PageLoading } from "./components";
+import NotFoundPage from "./pages/not-found/NotFoundPage";
 
 const HomePage = lazy(() => import("./pages/home-page/HomePage"));
 const HotelPage = lazy(() => import("./pages/hotel-page/HotelPage"));
 
 function App() {
-	return (
-		<AppLayout>
-			<Suspense fallback={<PageLoading />}>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/hotels/:id" element={<HotelPage />} />
-				</Routes>
-			</Suspense>
-		</AppLayout>
-	);
+  return (
+    <AppLayout>
+      <Suspense fallback={<PageLoading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hotels/:id" element={<HotelPage />} />
+          <Route
+            path="*"
+            element={<NotFoundPage title="صفحه مورد نظر یافت نشد" />}
+          />
+        </Routes>
+      </Suspense>
+    </AppLayout>
+  );
 }
 
 export default App;
