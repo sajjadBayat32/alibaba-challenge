e59bd7bb87ce3c27c74a461e1b6d5d9fe17230c8
SajjadBayat <sajjad.bayat32@gmail.com>
2025-02-14 13:53:16 +0330

    feat: create development and production environments

--- src/services/hotel.api.ts
+++ src/services/hotel.api.ts
@@ -1,11 +1,10 @@
 import { Hotel, UserComment } from "../models";
 
-// const BaseUrl = "http://192.168.1.100:3001";
-const BaseUrl = "http://localhost:3001";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function fetchHotels() {
 	try {
-		const res = await fetch(`${BaseUrl}/hotels`);
+		const res = await fetch(`${API_BASE_URL}/hotels`);
 		if (!res.ok) throw new Error("Hotels not found");
 		const data = (await res.json()) as Hotel[];
 		return data;
@@ -17,7 +16,7 @@ export async function fetchHotels() {
 
 export async function fetchHotel(id: string | undefined) {
 	try {
-		const res = await fetch(`${BaseUrl}/hotels/${id}`);
+		const res = await fetch(`${API_BASE_URL}/hotels/${id}`);
 		if (!res.ok) throw new Error("Hotel comment not found");
 		const data = (await res.json()) as Hotel;
 		return data;
@@ -29,7 +28,7 @@ export async function fetchHotel(id: string | undefined) {
 
 export async function fetchHotelComments(hotelId: string | undefined) {
 	try {
-		const res = await fetch(`${BaseUrl}/comments?hotelId=${hotelId}`);
+		const res = await fetch(`${API_BASE_URL}/comments?hotelId=${hotelId}`);
 		if (!res.ok) throw new Error("Hotel not found");
 		const data = (await res.json()) as UserComment[];
 		return data;
